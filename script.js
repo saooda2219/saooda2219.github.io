a3c844f4035958f1cf11f1adb9619f47d7860ee0
saooda2219 <56265222+saooda2219@users.noreply.github.com>
2019-10-08 11:04:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Saood Alzamzami"
+let userName = "saooda2219";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
